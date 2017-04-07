659e994a1750dc51ca8d01b2601e7f6e249c292a
awang <ywang348@usfca.com>
2017-04-07 00:18:23 -0700

    getoffline plot out

---
+++ r_shiny_world_development_indicator/get_data.R
@@ -0,0 +1,74 @@
+library(reshape)
+library(dplyr)
+
+
+root<-'~/git/data_visualization/r_shiny_world_development_indicator/'
+file_le<-paste(root,'API_SP.DYN.LE00.IN_DS2_en_csv_v2/API_SP.DYN.LE00.IN_DS2_en_csv_v2.csv',sep='')
+file_fertility<-paste(root,'API_SP.DYN.TFRT.IN_DS2_en_csv_v2/API_SP.DYN.TFRT.IN_DS2_en_csv_v2.csv',sep='')
+file_meta<-paste(root,'API_SP.DYN.TFRT.IN_DS2_en_csv_v2/Metadata_Country_API_SP.DYN.TFRT.IN_DS2_en_csv_v2.csv',sep='')
+
+get_data<-function(filepath){
+  # clean data, remove NAs, remove useless cols
+  
+  data<-read.csv(filepath, skip=4)
+  
+  names(data)<-append(names(data)[1:4],1960:2017)
+  data<-data[,1:59]
+  data<-data[complete.cases(data),]  
+  return(data)
+}
+
+get_meta_table<-function(){
+  # get meta_table, remove useless cols
+  meta_table<-read.csv(file_meta,header = TRUE,na.strings = c('','NA'))
+  meta_table<-meta_table[,1:3]
+  return(meta_table)
+}
+
+get_merged_data<-function(){
+  # return two dataframe in a list
+  
+  # get table- data_le, data_fertility, meta_table
+  data_le<-get_data(file_le)
+  data_fertility<-get_data(file_fertility)
+  meta_table<-get_meta_table()
+  # merge Region cols into data_le, data_fertility
+  df_fertility<-merge(data_fertility, meta_table, by='Country.Code')
+  df_le<-merge(data_le,meta_table, by='Country.Code')
+  # drop unnecessary cols (Indicator.Name, Indicator.Code, ..)
+  df_le<-df_le[,c(-3,-4, -61)]
+  df_fertility<-df_fertility[,c(-3,-4, -61)] 
+  results<-list(df_le,df_fertility)
+  return(results)
+}
+
+melt_table<-function(){
+  melted_le<-melt(df_le, id=c('Country.Code','Country.Name','Region'))
+  melted_le<-rename(melted_le, Year=variable,Life.Expectancy=value)
+  head(melted_le)
+  melted_fertility<-melt(df_fertility, id=c('Country.Code','Country.Name','Region'))
+  melted_fertility<-rename(melted_fertility, Year=variable,Fertility.Rate=value)
+  head(melted_fertility)
+  results<-list(melted_le,melted_fertility)
+  return(results)
+}
+
+
+results<-get_merged_data()
+df_le<-results[[1]]
+df_fertility<-results[[2]]
+
+melted_le<-melt_table()[[1]]
+melted_fertility<-melt_table()[[2]]
+
+super_data<-merge(melted_le, melted_fertility, on=c('Country.Code','Country.Name','Region', 'Year'))
+head(super_data,20)
+
+make_plot<-function(yr){
+  library(ggplot2)
+  g<-ggplot(subset(super_data, Year==yr), aes(Life.Expectancy, Fertility.Rate, color=Region))
+  g+geom_point()#+ theme(legend.position="none")
+  #str(super_data)  
+}
+
+make_plot(1960)
---
+++ r_shiny_world_development_indicator/server.R
@@ -0,0 +1,15 @@
+# Create a shiny app that gets as close as you can to this plot.
+# 
+# https://goo.gl/73vR7f (Links to an external site.)  
+# (Life expectancy versus fertility rate)
+
+# It has to have the following features:
+#  1) Get the name of the country when you hover or click
+#  2) Be able to select continents
+#  3) Show the time series running 
+
+
+# select continent
+
+# time series (data of main data)
+
